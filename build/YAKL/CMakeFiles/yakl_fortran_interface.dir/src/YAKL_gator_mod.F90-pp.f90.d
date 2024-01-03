16039142f69f08284574f49c55d858a177e4b62f
anyin233 <shizukuprprpr@gmail.com>
2024-01-03 14:36:02 +0800

    misc: update .gitignore

--- build/YAKL/CMakeFiles/yakl_fortran_interface.dir/src/YAKL_gator_mod.F90-pp.f90.d
+++
@@ -1,2 +0,0 @@
-YAKL/CMakeFiles/yakl_fortran_interface.dir/src/YAKL_gator_mod.F90-pp.f90: \
- /home/export/online1/mdt00/shisuan/swustc/yyw/swuc-yakl-workflow/porting/build/YAKL/src/YAKL_gator_mod.F90
