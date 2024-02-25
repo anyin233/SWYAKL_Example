69c062121eacd114bec82c279f23bfd2fcff6cdb
anyin233 <shizukuprprpr@gmail.com>
2024-02-25 13:37:59 +0800

    misc: add toolchain file for swumpicxx

---
+++ cmake/toolchains/sw64mpi.cmake
@@ -0,0 +1,19 @@
+#
+# Set the target operating system
+#
+set(CMAKE_SYSTEM_NAME Linux CACHE STRING "Cross-compiling for Sunway")
+set(CMAKE_SYSTEM_PROCESSOR "sw64")
+set(SW64 TRUE)
+
+set_property(GLOBAL PROPERTY TARGET_SUPPORTS_SHARED_LIBS FALSE)
+
+#
+# Set the compiler
+#
+set(CMAKE_C_COMPILER swumpicc)
+set(CMAKE_CXX_COMPILER swumpicxx)
+set(CMAKE_Fortran_COMPILER swgfortran)
+
+set(CMAKE_CROSSCOMPILING_EMULATOR "${CMAKE_SOURCE_DIR}/tools/bsub.sh")
+add_compile_options(-mhybrid-coding -mieee -mftz -msimd)
+add_link_options(-mhybrid)
