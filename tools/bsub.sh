76c689cbd88472d84d369865a9987d3deced084f
anyin233 <shizukuprprpr@gmail.com>
2024-03-16 23:48:43 +0800

    feat: add fortran example

--- tools/bsub.sh
+++ tools/bsub.sh
@@ -2,4 +2,4 @@
 exec_name=$(basename $1)
 
 # export GATOR_DISABLE="T"
-bsub -I -b -o ${exec_name}.log -q q_sw_expr -n 1 -cgsp 64 -share_size 13120 -cross_size 1024  $1
+bsub -I -b -o ${exec_name}.log -q q_sw_expr -n 1 -cgsp 64 -share_size 13120 -cross_size 1024 -cache_size 128  $1
