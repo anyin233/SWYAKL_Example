18e2004a34fe45d02fbd2de605339b62a6914712
cydia2001 <shizukuprprpr@gmail.com>
2024-03-17 00:10:06 +0800

    misc: change CPU backend to OPENMP

--- buildCPU.sh
+++ buildCPU.sh
@@ -3,7 +3,7 @@
 # Set the path to the CUDA toolkit
 export PATH=$PATH:/usr/local/cuda-11.7/bin
 
-cmake -DYAKL_ARCH="" \
+cmake -DYAKL_ARCH="OPENMP" \
 	-DYAKL_CXX_FLAGS="-g -O3" \
 	-DYAKL_F90_FLAGS="-g -O3" \
 	-DCMAKE_EXPORT_COMPILER_COMMANDS=ON \
