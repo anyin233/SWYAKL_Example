230cd8245c1c728219a80e9f299c27d13b3ad2ae
anyin233 <shizukuprprpr@gmail.com>
2024-03-16 23:52:42 +0800

    feat: add fortran example

---
+++ src/CPP/cpe_adapter.cpp
@@ -0,0 +1,4 @@
+// This code is use for seperate CPE code with SPE code
+// If integrate this header into YAKL library, swucxx will compile mutex, thread... library for CPE
+// which don't exist in CPE's std library
+#include "YAKL.h"
---
+++ src/Fortran/main.f90
@@ -0,0 +1,32 @@
+
+program wrapper
+  use gator_mod
+  implicit none
+  integer, parameter :: n = 1024
+  real :: a(n), b(n), c(n)
+  integer :: i
+
+  interface init
+    subroutine add(a, b, c, n) bind(C,name="add")
+      use iso_c_binding
+      real, dimension(*) :: a, b, c
+      integer :: n
+    end subroutine
+  end interface
+  do i = 1, n
+    c(i) = 11
+  end do
+  
+  call gator_init()
+
+  call add(a,b,c,n)
+  
+
+  if (sum(a) / n /= 3 .or. sum(b) / n /= 1 .or. sum(c) / n /= 2 ) then
+    write(*,*) 'ERROR: wrong sum'
+    stop
+  endif
+
+  call gator_finalize()
+
+end program wrapper
